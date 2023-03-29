b579fcc53385cccbfdc3072858376fce5f1d40c8
ram718 <ramsahith65@gmail.com>
2023-03-29 21:33:21 +0530

    Cart page is built along with Product Item component

---
+++ Frontend/src/Components/Cart/Cart.breadCrumb.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CartBreadCrumb = () => {
+  return <div></div>;
+};
+
+export default CartBreadCrumb;
--- Frontend/src/Routes/Allroutes.jsx
+++ Frontend/src/Routes/Allroutes.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import Address from "../Pages/Address";
+import Cartpage from "../Pages/Cartpage";
+import Payment from "../Pages/Payment";
+import Summary from "../Pages/Summary";
 
 function Allroutes() {
   return (
     <Routes>
       {/* TODO: add routes here */}
+      <Route path="/cart" element={<Cartpage />}></Route>
+      <Route path="/cart/address" element={<Address />}></Route>
+      <Route path="/cart/payment" element={<Payment />}></Route>
+      <Route path="/cart/summary" element={<Summary />}></Route>
     </Routes>
   );
 }
